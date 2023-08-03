449a4d9e62c028411787d091bc254bc04da7053f
sasender <78895362+sasender@users.noreply.github.com>
2023-08-03 09:06:36 +0530

    Update list-comprehensive-method.py

--- stark/python-example/list-comprehensive-method.py
+++ stark/python-example/list-comprehensive-method.py
@@ -18,3 +18,27 @@ sasender = [i for i in range(0,25)]
 
 print (sasender)
 
+####################################################
+# print odd-methods
+
+list_odd_methods = [i for i in range(1,15, 2)
+
+print (list_odd_methods)
+
+################################################
+list_odd_method1 = [i for i in range(0,15,3)]
+
+print (list_odd_method1)
+
+##################################################
+
+list_odd_method = [i for i in range(0,15) if i % 2 == 1]
+
+print (list_odd_method)
+
+list_even_method = [i for i in range(0,15) if i % 2 == 0]
+
+print (list_even_method)
+
+############################################################
+
